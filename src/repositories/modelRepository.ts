7dbe6643720151b4d25935ce3fd75118d93bc84d
NIkita Belobrov <belobrov.n@gmail.com>
2018-07-30 15:06:29 +0300

    fix

--- src/repositories/modelRepository.ts
+++ src/repositories/modelRepository.ts
@@ -255,7 +255,7 @@ export abstract class ModelRepository<
    */
   protected abstract fetchModel(id: string): Promise<any> | null;
 
-  protected abstract fetchList(name: string, consume?: (models: any[]) => void): Promise<any[]>;
+  protected abstract fetchList(name: string, consume: (models: any[]) => void): Promise<any[]>;
   protected abstract create(saveModel: CreateRequest): Promise<any>;
   protected abstract update(saveModel: UpdateRequest): Promise<any>;
   protected abstract deleteOne(model: T): Promise<any>;
